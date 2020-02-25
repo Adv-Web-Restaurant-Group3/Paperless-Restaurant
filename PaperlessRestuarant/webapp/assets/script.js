6525cd9123722da0e8d3eeccb139506d82ea6b47
Jake <38831456+17083659@users.noreply.github.com>
2020-02-25 19:48:13 +0000

    jake

--- PaperlessRestuarant/webapp/assets/script.js
+++ PaperlessRestuarant/webapp/assets/script.js
@@ -8,6 +8,24 @@ var orderDate = [{
         "itemName" : "Chicken Chow Mein",
         "quantity" : 1,
         "notes" : "notes"
+    },
+    {
+        "itemNum" : 2,
+        "itemName" : "Chicken Chow Mein",
+        "quantity" : 1,
+        "notes" : "notes"
+    }],
+    "status" : "Cooking",
+    "price" : 50
+},
+{
+    "orderNum" : 1,
+    "orderTime" : 1300,
+    "item" : [{
+        "itemNum" : 1,
+        "itemName" : "Chicken Chow Mein",
+        "quantity" : 1,
+        "notes" : "notes"
     }],
     "status" : "Cooking",
     "price" : 50
@@ -21,45 +39,88 @@ function updateOrder() {
 
     var sectionA = document.getElementById("section-a");
 
-    sectionA.innerHTML = "";
+    var content = "";
 
     for(var i=0;i<orderDate.length;i++) {
         orderNum = orderDate[i].orderNum;
         orderTime = orderDate[i].orderTime;
 
-        
-        for(var x=0;x<orderDate[i].item.length;x++) {
-            itemNum = orderDate[i].item[x].itemNum;
-            itemName = orderDate[i].item[x].itemName;
-            quantity = orderDate[i].item[x].quantity;
-            notes = orderDate[i].item[x].notes;
-        }
-
         status = orderDate[i].status;
         price = orderDate[i].price;
 
-        sectionA.innerHTML += `
+        content += `
         <div class='container-a'>
         <span class='text-a'>Order ${orderNum}</span>
         <span class='text-b'>Est wait time<br><span class='estWait'>${orderTime}</span></span>
         <span class='text-c'>Time waiting<br><span class='waitTime'>${orderTime}</span></span>
         <span class='text-d'>${status}</span>
         <span class='text-e'>${orderDate[i].item.length} Items</span>
-        <div class='openBox'>
-        <div class='container-b'>
-        <span class='itemName'><b>${itemNum}.</b>${itemName}</span>
-        <span class='itemQuantity'>x${quantity}</span>
-        <span class='notes'>${notes}</span>
-        </div>
+        <span class='dropDown' data-value='${i}'>open</span>
+        <div class='openBox' style='display:none'>`;
+        
+        for(var x=0;x<orderDate[i].item.length;x++) {
+            itemNum = orderDate[i].item[x].itemNum;
+            itemName = orderDate[i].item[x].itemName;
+            quantity = orderDate[i].item[x].quantity;
+            notes = orderDate[i].item[x].notes;
+
+            content += `
+            <div class='container-b'>
+            <span class='itemName'><b>${itemNum}.</b>${itemName}</span>
+            <span class='itemQuantity'>x${quantity}</span>
+            <span class='notes'>${notes}</span>
+            </div>
+            `;
+        }
+
+
+        content += `
         <div class='served'>Starters Served</div>
         <div class='edit'>edit</div>
         </div>
         </div>
         `;
 
+        sectionA.innerHTML = content;
+        
+
+
 
     }
     
 
 
+}
+
+
+
+
+var myFunction = function() {
+    var attribute = this.getAttribute("data-myattribute");
+    alert(attribute);
+};
+
+var dropDown = document.getElementsByClassName("dropDown");
+var openBox = document.getElementsByClassName("openBox");
+for (var i = 0; i < dropDown.length; i++) {
+    dropDown[i].addEventListener('click', function() {
+        var num = this.getAttribute("data-value");
+        var show = true;
+        if (openBox[num].style.display === "none") {
+            show = false;
+        }
+
+        for (var x = 0; x < openBox.length; x++) {
+            openBox[x].style.display = "none";
+        }
+        for (var x = 0; x < openBox.length; x++) {
+            dropDown[x].innerHTML = "Open";
+        }
+        
+        if(!show) {
+            openBox[num].style.display = "block";
+            this.innerHTML = "Close";
+        }
+        
+    });
 }

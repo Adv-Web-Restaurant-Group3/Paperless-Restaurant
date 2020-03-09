f278af0b4d5e686bbe75a3407bda59cb8431abb7
robflynnyh <16026255@stu.mmu.ac.uk>
2020-03-09 12:52:37 +0000

    Update server.js

--- PaperlessRestuarant/server.js
+++ PaperlessRestuarant/server.js
@@ -5,7 +5,8 @@ var app = express();
 var server = http.createServer(app);
 var fs = require("fs");
 const io = require("socket.io")(server);
-app.use(express.static("webapp"));
+
+app.use(express.static(__dirname+"/webapp"));
 
 /**
  * =================================

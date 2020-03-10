401b539c40ded376eab6aaab3b1c3b55ba1706d8
robflynnyh <16026255@stu.mmu.ac.uk>
2020-03-10 13:20:29 +0000

    kitchen view

--- PaperlessRestuarant/webapp/js/chefClient.js
+++ PaperlessRestuarant/webapp/js/chefClient.js
@@ -23,8 +23,166 @@ let ordersObj = [
                 notes:"blah"
             }
         ]
+    },
+    {
+        orderID:123,
+        tableNo:3,
+        orderTime:"12:45",
+        items: [
+            {
+                itemNo:22,
+                name:"Chicken Chow Mein",
+                quantity:1,
+                notes:"Well Done"
+            },
+            {
+                itemNo:20,
+                name:"noodles",
+                quantity:1,
+                notes:"No Sauce"
+            },
+            {
+                itemNo:22,
+                name:"blah blah",
+                quantity:1,
+                notes:"blah"
+            }
+        ]
+    },
+    {
+        orderID:123,
+        tableNo:7,
+        orderTime:"12:45",
+        items: [
+            {
+                itemNo:22,
+                name:"Chicken Chow Mein",
+                quantity:1,
+                notes:"Well Done"
+            },
+            {
+                itemNo:20,
+                name:"noodles",
+                quantity:1,
+                notes:"No Sauce"
+            },
+            {
+                itemNo:22,
+                name:"blah blah",
+                quantity:1,
+                notes:"blah"
+            }
+        ]
+    },
+    {
+        orderID:123,
+        tableNo:1,
+        orderTime:"12:45",
+        items: [
+            {
+                itemNo:22,
+                name:"Chicken Chow Mein",
+                quantity:1,
+                notes:"Well Done"
+            },
+            {
+                itemNo:20,
+                name:"noodles",
+                quantity:1,
+                notes:"No Sauce"
+            },
+            {
+                itemNo:22,
+                name:"blah blah",
+                quantity:1,
+                notes:"blah"
+            }
+        ]
+    },
+    {
+        orderID:123,
+        tableNo:2,
+        orderTime:"12:45",
+        items: [
+            {
+                itemNo:22,
+                name:"Chicken Chow Mein",
+                quantity:1,
+                notes:"Well Done"
+            },
+            {
+                itemNo:20,
+                name:"noodles",
+                quantity:1,
+                notes:"No Sauce"
+            },
+            {
+                itemNo:22,
+                name:"blah blah",
+                quantity:1,
+                notes:"blah"
+            }
+        ]
+    },
+    {
+        orderID:123,
+        tableNo:5,
+        orderTime:"12:45",
+        items: [
+            {
+                itemNo:22,
+                name:"Chicken Chow Mein",
+                quantity:1,
+                notes:"Well Done"
+            },
+            {
+                itemNo:20,
+                name:"noodles",
+                quantity:1,
+                notes:"No Sauce"
+            },
+            {
+                itemNo:22,
+                name:"blah blah",
+                quantity:1,
+                notes:"blah"
+            }
+        ]
     }
-]
+];
+
+
+function buildItem(obj){
+    var orderTitle = obj.tableNo;
+    var item = $("<div class='orderBox'></div>");
+    // TOP BAR
+    var topBar = $("<div class='topBar'><div class='orderTitle'></div><span class='status'></span</div>");
+    topBar.find(".orderTitle").append("Table "+orderTitle);
+    topBar.find(".status").append("Waiting To Cook");
+    item.append(topBar);
+    item.append("<hr>");
+    //ITEMS
+    var itemsStr="<div class='orderItems'><ul>";
+    obj.items.forEach(el=>{
+        itemsStr+= "<li>";
+        itemsStr+= "<div class='orderItem'>" 
+        itemsStr+= "<span class='itemTXT'>"+el.name+"</span>";
+        itemsStr+= "<span class='itemNotes'>"+el.notes+"</span>";
+        itemsStr+= "</div>";
+        itemsStr+= "</li>";
+    });  
+    itemsStr+="</ul></div>";
+    item.append($(itemsStr));
+    //console.log(itemsStr);
+    $("#content").append(item);
+    //console.log(item.prop('outerHTML'));    
+}
+
+$(document).ready(()=>{
+    ordersObj.forEach(el=>{
+        buildItem(el);
+    });
+});
 
 /*
     <div class="orderBox">

8a9c85f91cd04e0ba1fe6b00359e6191a207eb3d
FrankDippnall <44262159+FrankDippnall@users.noreply.github.com>
2020-03-11 14:42:29 +0000

    Update KitchenClient.js

--- PaperlessRestuarant/webapp/js/KitchenClient.js
+++ PaperlessRestuarant/webapp/js/KitchenClient.js
@@ -4,35 +4,20 @@
 
 class KitchenClient {
     _current_orders = [];
-    _socket = io("/waiter");
+    _socket = io("/kitchen");
     _update_callback = null;
 
     get socket() { return this._socket; }
     get orders() { return this._current_orders; }
 
-    constructor() {
-        this.updateMenu();
-    }
-
-    updateMenu() {
-        this.socket.off("menu");
-        let client = this;
-        this.socket.on("menu", function (results) {
-            console.log(results);
-            client._items = results.items;
-            client._categories = results.categories;
-
-            if (client._menu_update_callback) client._menu_update_callback();
-        })
-        this.socket.emit("get_menu");
-    }
+    constructor() { }
 
     sync(timeout) {
         setInterval(this.update, timeout)
     }
 
     update() {
-        this.socket.emit("get_orders", { tableNum: this._table });
+        this.socket.emit("get_orders");
         this.socket.off("get_orders_result");
         let client = this;
         this.socket.on("get_orders_result", function (response) {

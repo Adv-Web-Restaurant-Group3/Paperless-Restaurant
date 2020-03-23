5b3a954429fe9dd2fdc9489d83120317f3d00b1a
FrankDippnall <44262159+FrankDippnall@users.noreply.github.com>
2020-03-23 16:16:46 +0000

    refactored HTML structure
    moved index to waiter and created landing page.

--- PaperlessRestuarant/webapp/js/AdminClient.js
+++ PaperlessRestuarant/webapp/js/AdminClient.js
@@ -6,47 +6,83 @@
 class AdminClient {
     _current_tables = [];
     _socket = io("/admin");
-    _update_callback = null;
+    _menu_update_callback = null;
+    _loggedIn = false;
+    _items = [];
+    _categories = [];
 
     get socket() { return this._socket; }
-    get tables() { return this._current_tables; }
 
     constructor() {
-        this.update();
     }
 
     sync(timeout) {
-        setInterval(() => this.update(), timeout);
+        setInterval(() => this.updateMenu(), timeout);
     }
 
-    update() {
-        //gets only orders that are SERVED and ready to pay.
-        this.socket.emit("get_tables");
-        this.socket.off("get_tables_result");
-        let client = this;
-        this.socket.on("get_tables_result", function (response) {
-            console.log(response)
+    onMenuUpdate(callback) {
+        this._menu_update_callback = callback;
+    }
+
+    login(password) {
+        this.socket.emit("login", { password: password });
+        this.socket.off("login_result");
+        this.socket.on("login_result", function (response) {
             if (response.success) {
-                client._update_tables(response.tables);
-                if (client._update_callback) client._update_callback(client.tables);
-            } else {
-                alert("server responded with an error: " + response.reason);
+                console.log("logged in successfully.");
+                this._loggedIn = true;
+            }
+            else {
+                console.log(response.reason)
             }
         });
     }
 
-    _update_tables(tables) {
-        for (let table of tables) {
-            for (let order of table.orders) {
-                //order.status = Object.keys(tablestatus)[order.status - 1].toLowerCase();
-                order.orderTime = new Date(order.orderTime);
-            }
-        }
-        this._current_tables = tables;
-        console.log(tables);
+    updateMenu() {
+        this.socket.off("menu");
+        let client = this;
+        this.socket.on("menu", function (results) {
+            console.log(results);
+            client._items = results.items;
+            client._categories = results.categories;
+
+            if (client._menu_update_callback) client._menu_update_callback();
+        })
+        this.socket.emit("get_menu");
     }
 
-    onUpdate(callback) {
-        this._update_callback = callback;
+    addMenuItem(item) {
+        //itemName, price, category, isVegetarian = false, isVegan = false, glutenFree = false, containsNuts = false
+        this.socket.emit("add_item", { item: item });
+        this.socket.off("add_item_result");
+        this.socket.on("add_item_result", function (response) {
+            if (response.success) {
+                console.log("successfully added item.");
+            }
+            else console.log(response.reason);
+        })
     }
+
+    removeMenuItem(itemNum) {
+        this.socket.emit("remove_item", { item: itemNum });
+        this.socket.off("remove_item_result");
+        this.socket.on("remove_item_result", function (response) {
+            if (response.success) {
+                console.log("successfully removed item " + itemNum);
+            }
+            else console.log(response.reason);
+        });
+    }
+
+    updateSales() {
+        this.socket.emit("get_sales");
+        this.socket.off("sales");
+        this.socket.on("sales", function (response) {
+            if (response.success) {
+                console.log(response);
+            }
+            else console.log(response.reason);
+        });
+    }
+
 }
